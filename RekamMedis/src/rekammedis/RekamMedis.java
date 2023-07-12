6d678a9c771d0e8251345cb2a0cf725d9ec0e6a3
Vrica Gede Penggalih <vricagedepenggalih@gmail.com>
2023-07-12 08:55:10 +0700

    Add files via upload

---
+++ RekamMedis/src/rekammedis/RekamMedis.java
@@ -0,0 +1,8 @@
+package rekammedis;
+
+public class RekamMedis {
+    public static void main(String[] args) {
+        formPasien pasien = new formPasien();
+        pasien.setVisible(true);
+    }
+}
